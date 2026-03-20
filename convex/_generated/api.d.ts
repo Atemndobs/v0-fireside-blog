64280615f8a52d695590862514ee4d371f8f7cf5
Atemndobs <atemndobs@yahoo.com>
2026-03-20 16:30:22 +0100

    fix: Move finance page to correct route group (frontend)

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as finance from "../finance.js";
 import type * as migrations from "../migrations.js";
 import type * as mutations from "../mutations.js";
 import type * as queries from "../queries.js";
@@ -19,6 +20,7 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  finance: typeof finance;
   migrations: typeof migrations;
   mutations: typeof mutations;
   queries: typeof queries;
